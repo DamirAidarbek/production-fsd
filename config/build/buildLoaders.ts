1f6052860c2dbf8de733c549e7805944dbba12e0
DamirAidarbek <123254642+DamirAidarbek@users.noreply.github.com>
2023-12-05 10:07:49 +0600

    added jest, with first tests, added babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,6 +8,17 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     };
 
+    const babelLoader = {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            },
+        },
+    };
+
     const fileLoader = {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
@@ -45,6 +56,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     return [
         svgrLoader,
         fileLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ];
---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,22 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only one param', () => {
+        expect(classNames('classM')).toBe('classM');
+    });
+
+    test('with mods', () => {
+        expect(classNames('classM', { hovered: true, selected: true }))
+            .toBe('classM hovered selected');
+    });
+
+    test('with mods, additional', () => {
+        expect(classNames('classM', { hovered: true }, ['class1']))
+            .toBe('classM class1 hovered');
+    });
+
+    test('with false mods', () => {
+        expect(classNames('classM', { hovered: false, selected: true }, ['class1']))
+            .toBe('classM class1 selected');
+    });
+});
